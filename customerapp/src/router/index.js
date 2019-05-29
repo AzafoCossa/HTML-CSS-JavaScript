7f764efdd719790c076ad73c8d4b5dd14d3c577f
Azafo Cossa <azafosocial@gmail.com>
2019-05-29 11:00:46 +0200

    Created my components & installet vue-router & vue-resource

--- customerapp/src/router/index.js
+++ customerapp/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+  routes: [{
+    path: '/',
+    template: '<App/>',
+    component: {
+      App
     }
-  ]
+  }]
 })
